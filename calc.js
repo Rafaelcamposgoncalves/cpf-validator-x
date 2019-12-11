28a1cb3f21ef18592abad1945f8d7bd97c0c03de
Mozudos <mozudos@gmail.com>
2019-12-10 22:05:52 -0300

    update on calc.js and package.json

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 exports.calc = (a1, a2) => {
     let s=0;
-    const teste = a2.map( (value, i) => { s = s+(a1[i]*value); });
+    a2.map( (value, i) => { s = s+(a1[i]*value); });
     d = s/11;            
     r = Math.ceil(((d) - (Math.trunc(d))) * 10);
     r < 2 ? r = 0 : d1 = 11-r;
